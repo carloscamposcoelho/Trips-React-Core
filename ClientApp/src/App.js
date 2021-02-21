8b79277873309a2be4dfff8d5b840845326aa1cf
Carlos Coelho <carloscamposcoelho@gmail.com>
2021-02-21 12:55:25 -0300

    Debug via Chrome and formatting style improv

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -5,17 +5,17 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
-import './custom.css'
+import './custom.css';
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Route exact path="/" component={Home} />
+        <Route path="/counter" component={Counter} />
+        <Route path="/fetch-data" component={FetchData} />
       </Layout>
     );
   }
